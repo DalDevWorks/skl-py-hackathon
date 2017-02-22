5ab2676493664d0557ee0ac1849a203098e74060
Dylan S <dylstz@yahoo.ca>
2017-02-22 14:41:46 -0400

    Added models.py variable categories for data format.

--- profile_scraper/models.py
+++ profile_scraper/models.py
@@ -2,4 +2,23 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class UserProfile(models.Model):
+	twitterSource = models.CharField(max_length=100, unique=True)
+	firstName = models.CharField(max_length=50)
+	lastName = models.CharField(max_length=50)
+	domain = models.CharField(max_length=100)
+	company = models.CharField(max_length=100)
+	job = models.CharField(max_length=100)
+	title = models.CharField(max_length=100)
+	city = models.CharField(max_length=100)
+	state = models.CharField(max_length=100)
+	country = models.CharField(max_length=100)
+	influencer = models.CharField(max_length=16)
+	followers = models.IntegerField()
+	following = models.IntegerField()
+	description = models.TextField()
+	def __str__(self):
+		return self.handle
+
+
 # Create your models here.
