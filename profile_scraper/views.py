3af84730449b0623325a9d7d553b63b77bb49229
Connor Walsh <connorwalsh888@hotmail.com>
2017-02-22 17:11:39 -0400

    Refactoring the last commit. Replacing getProfile with getTwitterProfile, because having a class was overkill. Removed an unused template display.html

--- profile_scraper/views.py
+++ profile_scraper/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Profile
-from profile_scraper.getProfile import getProfile
+#from profile_scraper.getProfile import getProfile
+from profile_scraper.getTwitterProfile import getProfile
 from django.urls import reverse
 
-# Create your views here.
+"""
+Index displays form to enter a twitter handle
+"""
 def index(request):
     return render(request, 'profile_scraper/index.html')
 
+"""
+Lookup takes a post request from index.html and parses the twitter handle from the form
+Uses getProfile() to return a twitter profile object
+"""
 def lookup(request):
-    twitter = getProfile(request.POST['handle'])
-    name = twitter.profile.name
+    profile = getProfile(request.POST['handle'])
+    name = profile.name
     return render(request, 'profile_scraper/lookup.html', {'name': name})
-
-def display(request, name):
-    return render(request, 'profile_scraper/display.html', {'name':name})
