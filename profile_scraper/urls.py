09efcc4d6f1f0dfdd5508416d638effb70de9e81
Connor Walsh <connorwalsh888@hotmail.com>
2017-02-22 15:19:02 -0400

    Modified Profile model to add handle, added new migration for Profile, adding display template

--- profile_scraper/urls.py
+++ profile_scraper/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'profile_scraper'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<profile_id>[0-9]+)/$', views.display, name='display'),
 ]
--- profile_scraper/views.py
+++ profile_scraper/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from .models import Profile
 
 # Create your views here.
 def index(request):
     return render(request, 'profile_scraper/index.html')
+
+def lookup(request, handle):
+    #implement twitter api python program
+
+def display(request, profile_id):
+    profile = get_object_or_404(Profile, pk=profile_id)
+    return render(request, 'profile_scraper/display.html', {'profile': profile})
