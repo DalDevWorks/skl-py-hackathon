9d64f4a50751ffdd94c0b53ce807b0c548037c30
Connor Walsh <connorwalsh888@hotmail.com>
2017-02-23 13:42:48 -0400

    Modifying Profile model -Changed handle to twitterUserName -change fields job and title to one jobTitle -added datetime to default entry for twitterUserName -Added remaining fields to model from csv file

--- profile_scraper/models.py
+++ profile_scraper/models.py
@@ -1,16 +1,15 @@
 from __future__ import unicode_literals
-
+from datetime import datetime
 from django.db import models
 
 class Profile(models.Model):
     twitterSource = models.CharField(max_length=100, unique=True)
-    handle = models.CharField(max_length=100, unique=True, default="No Handle Entered")
+    twitterUserName = models.CharField(max_length=100, unique=True, default="No Handle Entered: " + str(datetime.now()))
     firstName = models.CharField(max_length=50)
     lastName = models.CharField(max_length=50)
     domain = models.CharField(max_length=100)
     company = models.CharField(max_length=100)
-    job = models.CharField(max_length=100)
-    title = models.CharField(max_length=100)
+    jobTitle = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
@@ -18,5 +17,12 @@ class Profile(models.Model):
     followers = models.IntegerField(null=True)
     following = models.IntegerField(null=True)
     description = models.TextField()
+    linkedin = models.CharField(max_length=100, unique=True, null=True)
+    cdSize = models.CharField(max_length=40, null=True)
+    cdIndustry = models.CharField(max_length=40, null=True)
+    hqCity = models.CharField(max_length=20, null=True)
+    hqState = models.CharField(max_length=20, null=True)
+    hqCountry = models.CharField(max_length=20, null=True)
+
     def __str__(self):
         return self.handle
