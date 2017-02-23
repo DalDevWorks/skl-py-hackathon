6f3063dd89f0d0b52a99b19f06d04ba0076592f9
Orjan Monsen <mons@dal.ca>
2017-02-23 18:00:02 -0400

    Added a redirect from localhost link to twitter classifier

--- twitter_classifier/urls.py
+++ twitter_classifier/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^profile_scraper/', include('profile_scraper.urls'))
+    url(r'^profile_scraper/', include('profile_scraper.urls')),
+    url(r'^', include('profile_scraper.urls'))
 ]
